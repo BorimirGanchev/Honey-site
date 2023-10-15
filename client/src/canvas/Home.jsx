16937461adba7c91f89f762045c3016b8f56712f
Borimir <borimir.ganchev@gmail.com>
2023-10-15 22:31:08 +0300

    progress in the left half of the front page

--- client/src/canvas/Home.jsx
+++ client/src/canvas/Home.jsx
@@ -5,20 +5,30 @@ function Home() {
   return (
     <div className='flex md:flex-row flex-col w-full items-center h-screen mx-auto justify-between'>
       <div className='basis-1/2 h-full'>
-        <div className='relative flex flex-col gap-10 top-20 left-20'>
+        <div className='relative flex flex-col gap-10 top-20 left-40'>
           <div className='relative'>
             <h3 className='text-3xl font-bold text-secondary'>Honey Bee</h3>
             <h6 className='text-base font-bold text-tertiary'>Pure From Nature</h6>
           </div>
           <div className='relative'>
-            <h1>Good Noney</h1>
-            <h1>Good Honey</h1>
+            <h1 className='text-5xl font-bold text-secondary' >Good Noney</h1>
+            <h1 className='text-5xl font-bold text-tertiary relative top-5 left-20' >Good Honey</h1>
           </div>
           <div>
-            <p>All our honey is single origin and we are proud practice sustainable beekeeping. Our honey is raw honey that is pure, unpesteurised, unprocessed and with no additives.</p>
+            <p className='border-b-2 border-secondary w-1/2 relative top-10' >All our honey is single origin and we are proud practice sustainable beekeeping. Our honey is raw honey that is pure, unpesteurised, unprocessed and with no additives.</p>
           </div>
-          <div>We would like to give you the best experience about taste of honey join us now!</div>
-          <div></div>
+          <div className='relative top-20'>
+            <p className='w-1/2'>We would like to give you the best experience about taste of honey join us now!</p>
+          </div>
+          <div className='relative top-32'>
+            <a href="#about">
+              <div className='flex items-center justify-center
+              bg-secondary w-1/6 h-10 rounded-md shadow-lg
+              shadow-secondary text-white
+              hover:bg-[#a94b11] hover:shadow-[#a94b11] '
+              >Explore Products</div>
+            </a>
+          </div>
         </div>
       </div>
       <div className='basis-1/2 bg-[#ffe6a1] h-full md:block hidden'>
